6fe58a9c90736e4ee9b1b70a4a290cc7a4285014
Eshayat Zamil Sharaf <imsharaf10@gmail.com>
2025-11-04 15:02:15 +0600

    Add files via upload

---
+++ Type conversion (Auto).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int n = 4;
+	float m = 3.1416;
+	
+	m += n;
+	
+	cout<< (m+1)<< endl;
+	cout << int(m+1);
+	
+	return 0;
+}
+
---
+++ complex operator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a = 10, b = 4;
+	
+	a += b;
+	cout << "value + : " <<a  ;
+	
+	return 0;
+	
+}
